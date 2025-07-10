abfa73e928e7ea44833c7a7569071fd91dad53cb
hiepph <hiepph@uht.com.vn>
2025-07-10 17:12:00 +0700

    create connector to postgres

--- connector.sh
+++ connector.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 curl.exe -X POST http://localhost:8093/connectors -H 'Content-Type: application/json' -d @mongo-connector.json
-curl.exe -X POST http://localhost:8093/connectors -H 'Content-Type: application/json' -d @elasticsearch-sink.json
+curl.exe -X POST http://localhost:8093/connectors -H 'Content-Type: application/json' -d @postgres-connector.json
